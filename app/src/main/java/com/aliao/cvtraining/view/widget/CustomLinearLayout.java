206ad81c26683cff3e9dcf12230bb635cfcb6dcf
阿廖 <liaolishuang@gmail.com>
2015-08-21 17:32:28 +0800

    探究ListView嵌套在ScrollView时显示不全的问题-根据测试view的onMeasure方法来推测onMeasure(int,int)参数的来源，并模拟实现该参数被传递进onMeasure前的获取方法。

--- app/src/main/java/com/aliao/cvtraining/view/widget/CustomLinearLayout.java
+++ app/src/main/java/com/aliao/cvtraining/view/widget/CustomLinearLayout.java
@@ -3,6 +3,8 @@ package com.aliao.cvtraining.view.widget;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.aliao.cvtraining.utils.L;
@@ -28,6 +30,73 @@ public class CustomLinearLayout extends LinearLayout {
         MeasureSpecUtil.printMode(heightMode, "height");
         MeasureSpecUtil.printMode(widthMode, "width");
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    public static int getChildMeasureSpec(int parentHeightSpec, int padding, int childHeight){//childHeight = lp.height
+
+        int parentHeightSpecMode = MeasureSpec.getMode(parentHeightSpec);
+        int parentHeightSpecSize = MeasureSpec.getSize(parentHeightSpec);
+
+        int size = Math.max(0, parentHeightSpecSize - padding);
+
+        int resultSize = 0;
+        int resultMode = 0;
+
+        switch(parentHeightSpecMode ){
+            case MeasureSpec.EXACTLY:
+
+                /**
+                 * child有精确值，那就由child自己决定
+                 * child如果设置match_parent，就用parent的可用最大值（一定填充到最大值）
+                 * child如果设置wrap_content，就用parent的可用最大值（但不一定用得到最大的值）
+                 */
+                resultSize = parentHeightSpecSize - padding;
+
+                if (childHeight >= 0){//有精确值
+                    //hild有精确值，那就由child自己决定
+                    resultSize = childHeight;
+                    resultMode = MeasureSpec.EXACTLY;
+
+                }else if (childHeight == ViewGroup.LayoutParams.MATCH_PARENT){
+                    resultSize = size;
+                    resultMode = MeasureSpec.EXACTLY;
+                }else if (childHeight == ViewGroup.LayoutParams.WRAP_CONTENT){
+                    resultSize = size;
+                    resultMode = MeasureSpec.AT_MOST;
+                }
+                break;
+
+            case MeasureSpec.AT_MOST:
+                if (childHeight > 0){
+                    //如果parent没有设置精确值，而child有精确值，就尊重child的设置，即使parent的高度比child设置的高度要小。
+                    resultSize = childHeight;
+                    resultMode = MeasureSpec.EXACTLY;
+                }else if (childHeight == ViewGroup.LayoutParams.MATCH_PARENT){
+                    //这个条件存在一定矛盾，parent的最终高度依赖于child的实际高度，但是child的高度设定又是填充parent的高度，所以最后实际显示的是child的实际高度
+                    //可能调用多次，第一次显示parent的高度，第二次显示实际占用的高度。
+                    resultSize = size;
+                    resultMode = MeasureSpec.AT_MOST;//最终变为EXACTLY
+                }else if (childHeight == ViewGroup.LayoutParams.WRAP_CONTENT){
+                    resultSize = size;
+                    resultMode = MeasureSpec.AT_MOST;
+                }
+                break;
+
+            case MeasureSpec.UNSPECIFIED:
+//                if (childHeight >= 0){
+//                    resultSize = childHeight;
+//                    resultMode = MeasureSpec.EXACTLY;
+//                }else if (childHeight == ViewGroup.LayoutParams.MATCH_PARENT){
+//                    resultSize = View.sUseZeroUnspecifiedMeasureSpec ? 0 : size;
+//                    resultMode = MeasureSpec.UNSPECIFIED;
+//                }else if (childHeight == ViewGroup.LayoutParams.WRAP_CONTENT){
+//                    resultSize = View.sUseZeroUnspecifiedMeasureSpec ? 0 : size;
+//                    resultMode = MeasureSpec.UNSPECIFIED;
+//                }
+                break;
+        }
+
+        return MeasureSpec.makeMeasureSpec(resultSize, resultMode);
     }
 
 
